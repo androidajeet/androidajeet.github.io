80f72dab73d6a86576232cdcb23d0e940ee2a14e
androidajeet <jeet.rgec@gmail.com>
2019-07-10 17:24:11 +0530

    Add files via upload

---
+++ precache-manifest.8cc13b5bd1cd58c3b92e48dad5886d8f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8711ec40c83acc5277303424e77a4729",
+    "url": "/SonyApp-React/index.html"
+  },
+  {
+    "revision": "389a6b16c8f95613bbef",
+    "url": "/SonyApp-React/static/css/2.57797527.chunk.css"
+  },
+  {
+    "revision": "61851b2748c5aaf12172",
+    "url": "/SonyApp-React/static/css/main.98cfff55.chunk.css"
+  },
+  {
+    "revision": "389a6b16c8f95613bbef",
+    "url": "/SonyApp-React/static/js/2.40c50b8b.chunk.js"
+  },
+  {
+    "revision": "61851b2748c5aaf12172",
+    "url": "/SonyApp-React/static/js/main.7d8c30a9.chunk.js"
+  },
+  {
+    "revision": "e6fefee8fd7cdfc679ea",
+    "url": "/SonyApp-React/static/js/runtime~main.f46ba01a.js"
+  },
+  {
+    "revision": "34230e4bbe3e289eb2bccacc356922ab",
+    "url": "/SonyApp-React/static/media/banner2.34230e4b.jpg"
+  },
+  {
+    "revision": "a89472b6e9a6aa0c58e3b0e345a449a7",
+    "url": "/SonyApp-React/static/media/logo.a89472b6.png"
+  },
+  {
+    "revision": "9a01454cef6b8d20b9b1d744594ecd9d",
+    "url": "/SonyApp-React/static/media/product.9a01454c.jpg"
+  }
+]);
